c39c1c2e847d0096333078782096b509ea9f1077
ttseng5897 <myvno@hotmail.com>
2014-07-11 11:42:03 +0800

    test

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms863');
+    define('DB_USER', 'simplecmsuser863');
+    define('DB_PASSWORD', '_5-A#NByZXA%');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'chtseng');
+    define('DEFAULT_ADMIN_PASSWORD', 'e020770');
+?>
